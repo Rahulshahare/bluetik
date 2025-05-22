5c73712c4f8953bcd8438e30227b751bbb69abd1
Rahulshahare <rahulshahare@gmail.com>
2025-05-22 20:47:56 +0530

    Can build

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import IOSModal from './IOSModal';
-import IOSButton from './IOSButton';
-import IOSCard from './IOSCard';
+import React from 'react';
 import MetaHorizonApp from './MetaHorizonApp';
 
 export default function App() {
-  const [modalVisible, setModalVisible] = React.useState(false);
   return (
     <MetaHorizonApp/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
